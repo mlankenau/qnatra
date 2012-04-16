9720531cc6bcf7510ccec6efe9eedcc1ffa4cd28
Rainer Jung <rainer.jung@friendscout24.com>
2012-04-16 08:49:14 +0200

    cleaned up whitespaces

--- lib/qnatra/processor.rb
+++ lib/qnatra/processor.rb
@@ -11,7 +11,7 @@ module Qnatra
     class << self
 
       # @group Code to process for every Message received
-  
+
       # Define processing instructions in a sintra style
       #
       # like:
@@ -19,15 +19,15 @@ module Qnatra
       #       puts "received msg #{msg[:payload]}"
       #    end
       def process(args, &block)
-        @processes ||= [] 
+        @processes ||= []
         @processes << args.merge({:block => block })
       end
-  
+
       # @endgroup
       # @group Setup of Handlers
-      
+
       # You can add several error-handlers giving the error-method a block to execute with every error.
-      # 
+      #
       # Here is a example how to use it.
       #    class ErrorLoggingProcessor < Qnatra::Processor
       #      error do |exception|
@@ -39,7 +39,7 @@ module Qnatra
         @error_handler ||= []
         @error_handler << block
       end
-  
+
       # For every successful handled Message, these handlers are called.
       #
       # Of course, it can be used for Logging:
@@ -53,7 +53,7 @@ module Qnatra
         @success_handler ||= []
         @success_handler << block
       end
-  
+
       # Handling of the three system events: startup, connect and error. On each, all system handlers are called with the event and some information-string.
       #
       # How about a Logging-Example:
@@ -63,31 +63,31 @@ module Qnatra
       #        Logger.info "System #{event}: #{message}"
       #      end
       #    end
-      def system_event(&block) 
+      def system_event(&block)
         @sysevent_handler ||= []
         @sysevent_handler << block
       end
 
       # @endgroup
-  
+
       def outbound(name, exchange, type)
         @outbounds ||= {}
         @outbounds[name] = {:exchange_name => exchange, :type => type }
       end
-  
+
       def post(outbound_name, payload, opts = {})
         outbound = @outbounds[outbound_name]
-        outbound[:exchange].publish(payload, opts) 
+        outbound[:exchange].publish(payload, opts)
       end
-  
+
       def ensync(&block)
-        @sync_queue << block  
+        @sync_queue << block
       end
-  
-  
+
+
       # start execution
-      # 
-      # for a single rabbitmq-host set 
+      #
+      # for a single rabbitmq-host set
       #   settings[:host] => hostname
       # for a cluster set
       #   settintgs[:hosts] => [host1, host2...]
@@ -101,23 +101,23 @@ module Qnatra
         @sync_queue ||= Queue.new
         sys_event :startup, 'startup'
         @stopped = false
-  
+
         pop_settings = settings.reject { |k,v| k != :ack }
-  
+
         begin
           next_host(settings)
-          
+
           sys_event :connect, "opening client on #{settings[:host]}:#{settings[:port]}"
           client = Bunny.new(settings)
-          client.start 
-  
+          client.start
+
           @outbounds ||= {}
-          @outbounds.each do |k,v| 
+          @outbounds.each do |k,v|
             exchange = client.exchange(v[:exchange_name], :type => v[:type])
             @outbounds[k][:exchange] = exchange
           end
-  
-          @processes.each do |p| 
+
+          @processes.each do |p|
             exchange = client.exchange( p[:exchange], :type => :topic )
             queue = client.queue( p[:queue] )
             if p[:key].is_a? Array
@@ -128,38 +128,38 @@ module Qnatra
               queue.bind(exchange, :key => p[:key])
             end
             p[:the_queue] = queue
-          end 
-  
+          end
+
           # endless loop and pop queues
-	  process_loop
+          process_loop
         rescue => e
-          #  we probably lost the connection to the queue 
+          #  we probably lost the connection to the queue
           # the next_host call at the beginning will select the next host
-  
+
           sys_event :error, "we received exception #{e.inspect}, we switch to next rabbit host and reconnect. Backtrace: #{e.backtrace}"
           sleep 1
           retry unless @stopped
         end
         client.stop
       end
-  
+
       def stop
         @stopped = true
       end
-  
+
       private
-  
+
       def process_loop
-        while !@stopped 
+        while !@stopped
           got_a_msg = false
-          @processes.each do |p| 
-            msg = p[:the_queue].pop :ack => p[:ack] 
+          @processes.each do |p|
+            msg = p[:the_queue].pop :ack => p[:ack]
             unless msg[:payload] == :queue_empty
               begin
                 duration = duration_of do
-                  p[:block].call msg 
+                  p[:block].call msg
                 end
-                p[:the_queue].ack if p[:ack] 
+                p[:the_queue].ack if p[:ack]
                 @success_handler.each do |h|
                   h.call :msg => msg, :queue => p[:queue], :exchange => p[:exchange], :topic => msg[:topic], :duration => duration
                 end
@@ -171,12 +171,12 @@ module Qnatra
               end
               got_a_msg = true
             end
-          end 
-          while !@sync_queue.empty? 
+          end
+          while !@sync_queue.empty?
             blck = @sync_queue.pop(true)
             blck.call
             got_a_msg = true
-          end 
+          end
           #sleep 0.01 unless got_a_msg # wait 100ms if all queues are empty
         end
       end
@@ -193,10 +193,10 @@ module Qnatra
         raise ArgumentError.new "Host address is empty" if settings[:host].nil?
         raise ArgumentError.new "Port is empty" if settings[:port].nil?
       end
-  
+
       def sys_event status, msg
         @sysevent_handler.each do |h|
-          h.call status, msg 
+          h.call status, msg
         end
       end
 
@@ -206,7 +206,7 @@ module Qnatra
         block.call
         (Time.new - start_time).to_f * 1000
       end
-  
+
     end
   end
-end 
+end
